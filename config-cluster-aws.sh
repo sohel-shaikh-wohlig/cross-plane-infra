c4dc0a2e1bcf4e406ef1eaca87f7110c3a41b671
Sohel <sohel.shaikh@wohlig.com>
2023-08-23 17:11:04 +0530

    config-cluster-aw.sh file created

---
+++ config-cluster-aws.sh
@@ -0,0 +1,42 @@
+export TEAM_NAME=$1
+
+export CLUSTER_NAME=$(\
+    kubectl get clusters \
+    --selector crossplane.io/composite=$TEAM_NAME \
+    --output jsonpath="{.items[0].metadata.name}")
+
+export KUBECONFIG=$PWD/kubeconfig.yaml
+
+mkdir -p $TEAM_NAME-apps
+
+touch $TEAM_NAME-apps/dummy
+
+cp -R team-app-reqs $TEAM_NAME-app-reqs
+
+aws eks --region us-east-1 \
+    update-kubeconfig \
+    --name $CLUSTER_NAME
+
+kubectl create namespace production
+
+argocd cluster add \
+    $(kubectl config current-context) \
+    --name $TEAM_NAME
+
+export SERVER_URL=$(kubectl config view \
+    --minify \
+    --output jsonpath="{.clusters[0].cluster.server}")
+
+cat orig/team-app-reqs.yaml \
+    | sed -e "s@server: .*@server: $SERVER_URL@g" \
+    | tee production/$TEAM_NAME-app-reqs.yaml
+
+cat orig/team-apps.yaml \
+    | sed -e "s@server: .*@server: $SERVER_URL@g" \
+    | tee production/$TEAM_NAME-apps.yaml
+
+git add .
+
+git commit -m "Team A apps"
+
+git push
